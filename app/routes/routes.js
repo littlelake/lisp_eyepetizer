0737c3a5656364ecf69c710f0bab70a965d79847
lisp <610031525@qq.com>
2020-01-13 16:27:34 +0800

    底部导航栏

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Image } from 'react-native'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 
 import { MainPage } from '../pages/home/index'
 import { VideoDetailPage, VideoPlayPage, SelectedPage } from '../pages/selected'
@@ -14,78 +14,90 @@ import { ratio, checkSource } from '../utils'
 
 // import Test from '../pages/Test'
 
-const TabStack = createMaterialBottomTabNavigator(
+const TabStack = createBottomTabNavigator(
   {
     SelectedPage: {
       screen: SelectedPage,
       navigationOptions: () => ({
-        tabBarLabel: '精选'
+        tabBarLabel: '精选',
+        tabBarIcon: ({ focused }) => {
+          if (focused) {
+            return (
+              <Image {...checkSource(require('../imgs/ic_tab_strip_icon_feed_selected.png'))} style={{ width: 35, height: 35, resizeMode: 'contain' }} />
+            )
+          }
+          return (
+            <Image {...checkSource(require('../imgs/ic_tab_strip_icon_feed.png'))} style={{ width: 35, height: 35, resizeMode: 'contain' }} />
+          )
+        }
       })
     },
     ExplorePage: {
       screen: ExplorePage,
       navigationOptions: () => ({
-        tabBarLabel: '发现'
+        tabBarLabel: '发现',
+        tabBarIcon: ({ focused }) => {
+          if (focused) {
+            return (
+              <Image {...checkSource(require('../imgs/ic_tab_strip_icon_category_selected.png'))} style={{ width: 35, height: 35, resizeMode: 'contain' }} />
+            )
+          }
+          return (
+            <Image {...checkSource(require('../imgs/ic_tab_strip_icon_category.png'))} style={{ width: 35, height: 35, resizeMode: 'contain' }} />
+          )
+        }
       })
     },
     FollowPage: {
       screen: FollowPage,
       navigationOptions: () => ({
-        tabBarLabel: '关注'
+        tabBarLabel: '关注',
+        tabBarIcon: ({ focused }) => {
+          if (focused) {
+            return (
+              <Image {...checkSource(require('../imgs/ic_tab_strip_icon_follow_selected.png'))} style={{ width: 35, height: 35, resizeMode: 'contain' }} />
+            )
+          }
+          return (
+            <Image {...checkSource(require('../imgs/ic_tab_strip_icon_follow.png'))} style={{ width: 35, height: 35, resizeMode: 'contain' }} />
+          )
+        }
       })
     },
     ProfilePage: {
       screen: ProfilePage,
       navigationOptions: () => ({
-        tabBarLabel: '我的'
+        tabBarLabel: '我的',
+        tabBarIcon: ({ focused }) => {
+          if (focused) {
+            return (
+              <Image {...checkSource(require('../imgs/ic_tab_strip_icon_profile_selected.png'))} style={{ width: 35, height: 35, resizeMode: 'contain' }} />
+            )
+          }
+          return (
+            <Image {...checkSource(require('../imgs/ic_tab_strip_icon_profile.png'))} style={{ width: 35, height: 35, resizeMode: 'contain' }} />
+          )
+        }
       })
     }
   },
   {
-    navigationOptions: (navigation) => ({
-      tabBarIcon: ({ focused, tintColor }) => {
-        const SELECTED = require('../imgs/ic_tab_strip_icon_feed.png')
-        const SELECTED_FOCUS = require('../imgs/ic_tab_strip_icon_feed_selected.png')
-
-        const EXPLORE = require('../imgs/ic_tab_strip_icon_category.png')
-        const EXPLORE_FOCUS = require('../imgs/ic_tab_strip_icon_category_selected.png')
-
-        const FOLLOW = require('../imgs/ic_tab_strip_icon_follow.png')
-        const FOLLOW_FOCUS = require('../imgs/ic_tab_strip_icon_follow_selected.png')
-
-        const PROFILE = require('../imgs/ic_tab_strip_icon_profile.png')
-        const PROFILE_FOCUS = require('../imgs/ic_tab_strip_icon_profile_selected.png')
-        const { routeName } = navigation.state
-        let iconName
-        if (routeName === 'SelectedPage') {
-          iconName = SELECTED
-        } else if (routeName === 'ExplorePage') {
-          iconName = EXPLORE
-        } else if (routeName === 'FollowPage') {
-          iconName = FOLLOW
-        } else if (routeName === 'ProfilePage') {
-          iconName = PROFILE
-        }
-        if (focused) {
-          iconName = `${iconName}_FOCUS`
-        }
-        return (
-          <Image {...checkSource(iconName)} style={{ width: 35, height: 35, resizeMode: 'contain' }} />
-        )
-      }
-    })
-  },
-  {
     initialRouteName: 'SelectedPage',
-    activeColor: '#03C2A6',
-    inactiveColor: '#999999',
-    barStyle: {
-      height: 50,
-      backgroundColor: '#fff',
-      paddingBottom: 3,
-      paddingTop: 0,
-      borderTopWidth: 0.5 / ratio,
-      borderTopColor: '#ededed'
+    tabBarOptions: {
+      activeTintColor: '#03C2A6',
+      inactiveTintColor: '#999',
+      showIcon: true,
+      labelStyle: {
+        fontSize: 12
+      },
+      style: {
+        height: 50,
+        backgroundColor: '#fff',
+        paddingBottom: 3,
+        paddingTop: 0,
+        borderTopWidth: 0.5 / ratio,
+        borderTopColor: '#ededed'
+      }
     }
   }
 )
